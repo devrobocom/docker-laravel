6db75a9489357494c299916462cc16ee73137f76
Yuksel Saliev <yuksel.saliev@gmail.com>
2018-11-07 09:12:05 +0200

    Up

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,13 @@
 
 service cron start
 
+if [ -z "$(ls -A /var/www)" ]; then
+    cd /var/www
+    composer global require laravel/installer
+    composer create-project --prefer-dist laravel/laravel .
+    chown www-data /var/www/ -R
+fi
+
 # Toggle xdebug
 if [ "false" == "$XDEBUG" ]; then
     sed -i "s/^/;/" /usr/local/etc/php/conf.d/xdebug.ini
